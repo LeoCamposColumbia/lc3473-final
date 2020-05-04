9a14480f2b56065a164fde7d44a278dcc2c941b1
Leonardo Campos <lc3473@columbia.edu>
2020-05-04 00:56:50 -0400

    Finished game

--- final/app.py
+++ final/app.py
@@ -33,10 +33,6 @@ def classes():
 def game():
     return render_template("game.html")
 
-@app.route("/gamepage/investigate")
-def investigate():
-    return render_template("investigate.html")
-
 @app.route("/gamepage/bed")
 def bed():
     return render_template("bed.html")
@@ -45,13 +41,77 @@ def bed():
 def leave():
     return render_template("leave.html")
 
-@app.route("/gamepage/bed/window")
-def dead():
-    return render_template("bed.html")
+@app.route("/gamepage/bed/leave/stab")
+def stab():
+    return render_template("stab.html")
 
-@app.route("/win")
-def win():
-    return render_template("win.html")
+@app.route("/gamepage/bed/leave/stab/downstairs")
+def downstairs():
+    return render_template("downstairs.html")
+
+@app.route("/gamepage/bed/leave/stab/downstairs/stealth")
+def stealth():
+    return render_template("stealth.html")
+
+@app.route("/gamepage/bed/leave/run")
+def run():
+    return render_template("run.html")
+
+@app.route("/gamepage/bed/leave/run/still")
+def still():
+    return render_template("still.html")
+
+@app.route("/gamepage/bed/leave/run/still/hide")
+def hide():
+    return render_template("hide.html")
+
+@app.route("/gamepage/bed/leave/run/still/fight")
+def fight():
+    return render_template("fight.html")
+
+@app.route("/gamepage/bed/leave/run/still/fight/break")
+def breaks():
+    return render_template("break.html")
+
+@app.route("/gamepage/bed/leave/run/still/fight/unlock")
+def unlock():
+    return render_template("unlock.html")
+
+@app.route("/gamepage/bed/leave/run/door")
+def door():
+    return render_template("door.html")
+
+@app.route("/gamepage/bed/window")
+def window():
+    return render_template("window.html")
+
+@app.route("/gamepage/investigate")
+def investigate():
+    return render_template("investigate.html")
+
+@app.route("/gamepage/investigate/swing")
+def swing():
+    return render_template("swing.html")
+
+@app.route("/gamepage/investigate/swing/basement")
+def basement():
+    return render_template("basement.html")
+
+@app.route("/gamepage/investigate/swing/basement/stealthy")
+def stealthy():
+    return render_template("stealthy.html")
+
+@app.route("/gamepage/investigate/runningman")
+def runningman():
+    return render_template("runningman.html")
+
+@app.route("/gamepage/earlydeparture")
+def earlydeparture():
+    return render_template("earlydeparture.html")
+
+@app.route("/gamepage/runenddeath")
+def runenddeath():
+    return render_template("runenddeath.html")
 
 #start the server
 if __name__ == "__main__":
